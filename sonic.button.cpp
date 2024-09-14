a881f7aef9de8c2d4bb9a9a661e129173007a758
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-14 12:29:05 -0500

    Update sonic.button.cpp

--- sonic.button.cpp
+++ sonic.button.cpp
@@ -2,7 +2,10 @@
 
 // hwnd = window handle of button provider. can be found at ButtonStatus.dll! 0x1070
 // msg = 0x8004 is WM_REDBUTTON
-// waram = 0 = OnRedButtonReleased, 1 = OnRedButtonPressed, 2 = Detected?, 3 =
-Removed?
+// waram = 0 = OnRedButtonReleased, 1 = OnRedButtonPressed, 2 = Detected?, 3 = Removed?
 // Param = unused
+
+int main() {
 SendMessage ( (HWND) 0x1000b02c8, 0x8004, 2, 0);
+return 0;
+}
